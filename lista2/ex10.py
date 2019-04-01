69017ef3fd900fd3868c77e2a03102f8c047b2dd
Germano Barcelos <gegenbarcelos@gmail.com>
2019-04-01 20:02:32 -0300

    ex10 e ex12

--- lista2/ex10.py
+++ lista2/ex10.py
@@ -1,17 +1,11 @@
-import math as m
-qtdTermos = 0
-denominador = 2
-serie = 0
-
-while qtdTermos < 51:
-    denominador += 1
-    if denominador % 2 != 0:
-        if qtdTermos + 1 % 2 != 0:
-            serie += -1/denominador ** 3
-            qtdTermos += 1
-        else:
-            serie += +1/denominador ** 3
-            qtdTermos += 1
-serie += 1
+serie = 1
+for i in range(3, 54):
+    if i == 3:
+        denominador = i
+    elif denominador % 2 == 0:
+        denominador += 3
+    else:
+        denominador += 2
+    serie += ((-1)**(i))*(1/(denominador ** 3))
 pi = (serie * 32) ** (1./3)
 print(pi)
--- lista2/ex12.py
+++ lista2/ex12.py
@@ -1,14 +1,9 @@
-qtdTermos = 0
 denominador = 10
 numerador = 480
 serie = 0
 
-while qtdTermos < 30:
-    if qtdTermos + 1 == 0:
-        serie += -numerador/denominador
-    else:
-        serie += numerador/denominador
-    qtdTermos += 1
+for i in range (30):
+    serie += ((-1)**(i))*(numerador/denominador)
     numerador -= 5
     denominador += 1
 print(serie)
