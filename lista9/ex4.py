4d4124ed037f72a4595295a4d77dc55f75020db6
Germano Barcelos <gegenbarcelos@gmail.com>
2019-06-02 12:50:45 -0300

    finishing ex4.py

--- lista9/ex4.py
+++ lista9/ex4.py
@@ -3,13 +3,13 @@ file2 = input()
 mat = {}
 
 with open(file1, 'r') as f, open(file2, 'r') as g:
-  notas = list(g.readline().split(' '))
-  mat[f.readline()] = notas
-
+  notas = g.readlines()
+  nomes = f.readlines() 
+  for i in range(len(nomes)):
+    mat[nomes[i][:len(nomes[i])]] = notas[i][:len(notas[i])]
 with open('ex4.txt', 'w') as f:
   for k, v in mat.items():
     f.write(k)
-    f.write('\n')
     for x in v:
-      f.write(x + ' ')
+      f.write(x)
     f.write('\n')
